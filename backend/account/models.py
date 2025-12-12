734ce31f2e8ca5456a0644fd0303f030b9b3b32a
taku820801 <takuchanpower@gmail.com>
2025-12-12 23:01:52 +0900

    Userモデル追加

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    User作成のメソッド
+
+    通常のUserもしくはsuperuserを作成
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("メールアドレスは必須です。")
+
+        email = self.normalize_email(email)
+        email = email.lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(
+    AbstractBaseUser,
+    PermissionsMixin,
+):
+    """
+    カスタムユーザーモデル
+    """
+
+    email = models.EmailField("メールアドレス", max_length=255, unique=True)
+    name = models.CharField("名前", max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    class Meta:
+        verbose_name = "ユーザー情報"
+        verbose_name_plural = "ユーザー情報"
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
