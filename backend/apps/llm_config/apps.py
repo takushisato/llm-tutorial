b3940e21246fa0f498d316814ac78dab52c26453
taku820801 <takuchanpower@gmail.com>
2025-12-12 23:07:32 +0900

    llmの設定appを追加して整理

---
+++ backend/apps/llm_config/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LlmConfigConfig(AppConfig):
+    name = "apps.llm_config"
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "account",
+    "apps.account",
+    "apps.llm_config",
 ]
 
 MIDDLEWARE = [
